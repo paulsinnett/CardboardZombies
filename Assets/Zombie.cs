5d01d27bff9129941a2ad414173df7e49d10b440
Paul Sinnett <paul.sinnett@gmail.com>
2015-05-21 14:09:36 +0100

    Initial commit
    basic zombie walking

---
+++ Assets/Zombie.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour
+{
+    public float tilt = 10f;
+    public float acceleration = 10f;
+    void Start()
+    {
+        StartCoroutine("Walk");
+    }
+
+    IEnumerator Walk()
+    {
+        for (;;)
+        {
+            float velocity = 0f;
+            for (float rotate = -tilt; rotate < tilt; rotate += velocity * Time.deltaTime)
+            {
+                transform.rotation = Quaternion.Euler(0f, 0f, rotate);
+                yield return null;
+                velocity += acceleration * Time.deltaTime;
+            }
+
+            velocity = 0f;
+            for (float rotate = tilt; rotate > -tilt; rotate -= velocity * Time.deltaTime)
+            {
+                transform.rotation = Quaternion.Euler(0f, 0f, rotate);
+                yield return null;
+                velocity += acceleration * Time.deltaTime;
+            }
+            
+            yield return null;
+        }
+    }
+}
