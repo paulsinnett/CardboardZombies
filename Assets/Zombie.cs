619052abc9631e0bb8a1659edc5f11316ffe6605
Paul Sinnett <paul.sinnett@gmail.com>
2015-05-22 09:19:37 +0100

    horror face
    fiddled with materials, made the head / face a block, created a
    minecraft-ish green cardboard material

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -5,9 +5,13 @@ public class Zombie : MonoBehaviour
 {
     public float tilt = 10f;
     public float acceleration = 10f;
+
+    Transform head;
+
     void Start()
     {
         StartCoroutine("Walk");
+        head = transform.Find("Root/Head");
     }
 
     IEnumerator Walk()
@@ -22,6 +26,8 @@ public class Zombie : MonoBehaviour
                 velocity += acceleration * Time.deltaTime;
             }
 
+            head.localRotation = Quaternion.Euler(0f, 0f, tilt);
+
             velocity = 0f;
             for (float rotate = tilt; rotate > -tilt; rotate -= velocity * Time.deltaTime)
             {
@@ -29,8 +35,8 @@ public class Zombie : MonoBehaviour
                 yield return null;
                 velocity += acceleration * Time.deltaTime;
             }
-            
-            yield return null;
+
+            head.localRotation = Quaternion.Euler(0f, 0f, -tilt);
         }
     }
 }
