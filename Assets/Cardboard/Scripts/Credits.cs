eb4384cc16fc927c82961accfabb9d1c6712764f
Paul Sinnett <paul.sinnett@gmail.com>
2015-08-10 14:03:32 +0100

    credits work in progress

---
+++ Assets/Cardboard/Scripts/Credits.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Credits : MonoBehaviour
+{
+    ScrollRect credits;
+    float time;
+
+    void Start()
+    {
+        credits = GetComponent<ScrollRect>();
+        time = 0f;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        credits.verticalNormalizedPosition = Mathf.Clamp01(1 - time);
+    }
+}
